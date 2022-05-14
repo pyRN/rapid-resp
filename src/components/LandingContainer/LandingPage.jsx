bde1cc220a6a56e48be8684fdf5185e511c62e2f
Jacob <jacobmayeux@gmail.com>
2022-05-14 13:12:07 -0500

    Initial creation of LandingPage component

---
+++ src/components/LandingContainer/LandingPage.jsx
@@ -0,0 +1,17 @@
+const LandingPage = () =>{
+    return (<main>
+        <div className="hero">
+            <h1>Rapid Response</h1>
+        </div>
+        <div className="row">
+            <div className="card">
+                <h2>Code FAST</h2>
+            </div>
+            <div className="card">
+                <h2>Code Blue</h2>
+            </div>
+        </div>
+    </main>)
+}
+
+export default LandingPage
