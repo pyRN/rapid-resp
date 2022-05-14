5529fd7cf1ac0234f6b94df52f3f7807c67a2967
Jacob <jacobmayeux@gmail.com>
2022-05-14 12:33:43 -0500

    Added Routes for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <title>Rapid Response</title>
-      </header>
+      <Router>
+        <Route path="/"><LandingPage/></Route>
+        <Route path="/codeFast"><CodeFastContainer/></Route>
+        <Route path="/codeBlue"><CodeBlueContainer/></Route>
+        <Route path="/log"><LogContainer/></Route>
+      </Router>
     </div>
   );
 }
