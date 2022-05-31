d372619a50360f51e992a02fc7863da0e275796b
Jacob <jacobmayeux@gmail.com>
2022-05-31 16:22:38 -0500

    Added LogContainer, no functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LandingPage from "./components/LandingContainer/LandingPage"
 import CodeFastContainer from "./components/CodeFastContainer/CodeFastContainer"
 import CodeBlueContainer from "./components/CodeBlueContainer/CodeBlueContainer"
 import StemiContainer from "./components/StemiContainer/StemiContainer"
-// import LogContainer from "./components/LogContainer"
+import LogContainer from "./components/LogContainer/LogContainer"
 
 export default function App() {
   return (
@@ -19,7 +19,7 @@ export default function App() {
   <Route path="/codeFast" element={<CodeFastContainer />}/>
   <Route path="/codeBlue" element={<CodeBlueContainer />}/>
   <Route path="/stemi" element={<StemiContainer />}/>
-  {/* <Route path="/log" element={<LogContainer />}/> */}
+  <Route path="/log" element={<LogContainer />}/>
 </Routes>
 </BrowserRouter>
   );
---
+++ src/components/LogContainer/LogContainer.jsx
@@ -0,0 +1,9 @@
+const LogContainer = () => {
+  return (
+    <main className="main-code-fast-container">
+      <h2>Log Container</h2>
+    </main>
+  );
+};
+
+export default LogContainer;
