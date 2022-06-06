5440dfb543c3adb8667a45bf2bccf0d1f78bc1f2
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-06 07:20:17 -0500

    Moved global styles to styles.css, removed index.css/app.css, imported new styles.css to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import './App.css';
-import {   BrowserRouter,
-  Routes,
-  Route} from "react-router-dom";
+import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Components
-import LandingPage from "./components/LandingContainer/LandingPage"
-import CodeFastContainer from "./components/CodeFastContainer/CodeFastContainer"
-import CodeBlueContainer from "./components/CodeBlueContainer/CodeBlueContainer"
-import StemiContainer from "./components/StemiContainer/StemiContainer"
-import LogContainer from "./components/LogContainer/LogContainer"
+import LandingPage from "./components/LandingContainer/LandingPage";
+import CodeFastContainer from "./components/CodeFastContainer/CodeFastContainer";
+import CodeBlueContainer from "./components/CodeBlueContainer/CodeBlueContainer";
+import StemiContainer from "./components/StemiContainer/StemiContainer";
+import LogContainer from "./components/LogContainer/LogContainer";
 
 export default function App() {
   return (
-
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<LandingPage />}/>
-  <Route path="/codeFast" element={<CodeFastContainer />}/>
-  <Route path="/codeBlue" element={<CodeBlueContainer />}/>
-  <Route path="/stemi" element={<StemiContainer />}/>
-  <Route path="/log" element={<LogContainer />}/>
-</Routes>
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/codeFast" element={<CodeFastContainer />} />
+        <Route path="/codeBlue" element={<CodeBlueContainer />} />
+        <Route path="/stemi" element={<StemiContainer />} />
+        <Route path="/log" element={<LogContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
