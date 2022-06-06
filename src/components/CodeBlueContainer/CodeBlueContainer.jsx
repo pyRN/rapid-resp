46ce02cf13bd1d94a96378882de4fc35731ddfc9
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-06 01:02:30 -0500

    Changed NavBar component to global, added nav elements for NavBar

--- src/components/CodeBlueContainer/CodeBlueContainer.jsx
+++ src/components/CodeBlueContainer/CodeBlueContainer.jsx
@@ -1,12 +1,54 @@
 //Components
-import NavBar from './NavBar'
+import Footer from "../Global/Footer";
+import NavBar from "../Global/NavBar";
 
-const CodeBlueContainer = () =>{
-    return (
-        <main className="main-code-blue-container">
-            <NavBar/>
-        </main>
-    )
-}
+const CodeBlueContainer = () => {
+  const oCodeBlueNavElements = {
+    oHeader: {
+      sText: "Code Blue",
+      sId: "#code-blue-header",
+    },
+    oClocks: {
+      bClock: true,
+      bCodeTime: true,
+    },
+    aNavBtns: [
+      {
+        sText: "CPR",
+        sId: "#cpr-nav",
+      },
+      {
+        sText: "Rhythm",
+        sId: "#rhythm-nav",
+      },
+      {
+        sText: "Quick Meds",
+        sId: "#quick-meds-nav",
+      },
+      {
+        sText: "Procedures",
+        sId: "#procedures-nav",
+      },
+      {
+        sText: "Medications",
+        sId: "#medications-nav",
+      },
+      {
+        sText: "Team",
+        sId: "#team-nav",
+      },
+      {
+        sText: "Notes",
+        sId: "#notes-nav",
+      },
+    ],
+  };
+  return (
+    <main className="main-code-blue-container">
+      <NavBar oElements={oCodeBlueNavElements} />
+      <Footer />
+    </main>
+  );
+};
 
-export default CodeBlueContainer
+export default CodeBlueContainer;
