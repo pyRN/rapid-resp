a0c04fa4b3619eb997b6a9b6292b4de8295f4fe0
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-09 08:26:32 -0500

    Fixed mobile menu on NavBar, Added log button, styled component

--- src/components/Global/NavBar.jsx
+++ src/components/Global/NavBar.jsx
@@ -1,47 +1,43 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import React, { useRef } from "react";
 import "./NavBar.css";
-//Components
-// import Clock from "./Clock";
-// import CodeTime from "./CodeTime";
 
 const NavBar = ({ oElements }) => {
+  const oHamburgerMenu = useRef(null);
+  const fnNavigate = useNavigate();
+
+  const fnToggleMobileMenu = (event) => {
+    event.preventDefault();
+    oHamburgerMenu.current.classList.toggle("open");
+  };
+  const fnOnClick = (event) => {
+    event.preventDefault();
+    fnNavigate("/log");
+  };
+
   return (
-    <nav className="navbar">
-      <h2 className="brand" id={oElements.oHeader.sId}>
-        {oElements.oHeader.sText}
-      </h2>
-      {/* <div className="clocks">
-        <Clock />
-        <CodeTime />
-      </div> */}
-
-      <div className="navbar-links">
-        {oElements.aNavBtns.map((oElement) => (
-          <a href={oElement.sId} className="nav-link" key={oElement.sId}>
-            {oElement.sText}
-          </a>
-        ))}
-
-        <Link to="/log" className="nav-link">
-          Log
-        </Link>
-      </div>
-      <div className="hamburger-menu">
-        <div className="top-hamburger-line"></div>
-        <div className="middle-hamburger-line"></div>
-        <div className="bottom-hamburger-line"></div>
-        <div className="hamburger-links">
+    <header className="navbar">
+      <h2 className="brand">{oElements.oHeader.sText}</h2>
+      <button className="btn log-btn" onClick={fnOnClick}>
+        Log
+      </button>
+      <div
+        className="hamburger-icon"
+        ref={oHamburgerMenu}
+        onClick={fnToggleMobileMenu}
+      >
+        <div className="hamburger-bar-1"></div>
+        <div className="hamburger-bar-2"></div>
+        <div className="hamburger-bar-3"></div>
+        <ul className="mobile-menu">
           {oElements.aNavBtns.map((oElement) => (
             <a href={oElement.sId} className="nav-link" key={oElement.sId}>
               {oElement.sText}
             </a>
           ))}
-          <Link to="/log" className="nav-link">
-            Log
-          </Link>
-        </div>
+        </ul>
       </div>
-    </nav>
+    </header>
   );
 };
 
